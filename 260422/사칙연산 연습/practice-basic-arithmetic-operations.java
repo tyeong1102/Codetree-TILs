da4b8847f2f6a275a7a67281f21f6db765e4cc14
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2026-04-22 13:09:05 +0900

    Title: 사칙연산 연습, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260422/사칙연산 연습/practice-basic-arithmetic-operations.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int a, b, c, div, res;
+
+        a = 5;
+        b = 30;
+        c = a + b;
+
+        div = c / 8;
+        res = c % 8;
+
+        System.out.println(div);
+        System.out.println(res);
+    }
+}
